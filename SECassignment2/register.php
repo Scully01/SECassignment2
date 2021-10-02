1ed5e5a4969d008918a983f15a3f4531be46ee69
Mitchell Trajcevski <s3723277@csitprdap01.int.its.rmit.edu.au>
2021-10-02 20:55:02 +1000

    file movement

--- SECassignment2/register.php
+++ SECassignment2/register.php
@@ -1,97 +1,83 @@
-<!DOCTYPE html>
-<!-- 
+<?php
+session_start();
+?>
+<html>
+
 <head>
-    <title>Secure Electronic Commerce</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="stylesheet.css">
-    <script type="text/javascript" src="register.js"></script>
-
-    <script type="text/javascript">
-        function addToCSVFile() {
-            var csvData = new Array();  // To collect the data
-            var csvFilePath = "user.csv"; // File name
-
-            // Collect General Information
-            csvData[0] = document.getElementById('username').value;
-            csvData[1] = document.getElementById('password').value;
-
-              var fso = new ActiveXObject('Scripting.FileSystemObject');
-            var oStream = fso.OpenTextFile(csvFilePath, 8, true, 0);
-            oStream.WriteLine(csvData.join(','));
-            oStream.Close();
-            clearData();
-            alert("Registered User Successfully!");
-     }
-
-     function clearData() {
-            document.getElementById('username').value = "";
-            document.getElementById('password').value = "";
-                     }
-    </script>
-</head> -->
-    <head>
-    <script type="text/javascript" src="register.js"></script>
-
-<script type="text/javascript">
-    function addToCSVFile() {
-        var csvData = new Array();  // To collect the data
-        var csvFilePath = "user.csv"; // File name
-
-        // Collect General Information
-        csvData[0] = document.getElementById('username').value;
-        csvData[1] = document.getElementById('password').value;
-
-          var fso = new ActiveXObject('Scripting.FileSystemObject');
-        var oStream = fso.OpenTextFile(csvFilePath, 8, true, 0);
-        oStream.WriteLine(csvData.join(','));
-        oStream.Close();
-        clearData();
-        alert("Registered User Successfully!");
- }
-
- function clearData() {
-        document.getElementById('username').value = "";
-        document.getElementById('password').value = "";
-                 }
-</script>
-<?php include 'header.php';?>
+    <?php include 'header.php'; ?>
 </head>
 
 <body>
-  
+    <?php
+    //Receive username from client side
+    $entered_username = $_POST['username'];
+    //Receive password from client side
+    $entered_password = $_POST['password'];
 
-    
+    if ($entered_username != "" & $entered_password != "") {
+        $register = 0;
+        //read users.txt line by line
+        foreach (file('users.txt') as $line) {
+            //split each line as two parts
+            list($username, $password) = explode(",", $line);
+            //verify if an exist user with the same username
+            if ($username == $entered_username) {
+                $register = 1;
+                break;
+            }
+        }
 
+        if ($register == 1) {
+            echo "The user exists!";
+        } else {
+            //open a file named "text.txt"
+            $file = fopen("users.txt", "a");
+            //insert this user into the users.txt file
+            fwrite($file, $entered_username . "," . $entered_password . "\n");
+            //close the "$file"
+            fclose($file);
+            echo "The user has been added to the users.txt";
+            header('Location: login.php');
+        }
+    } else {
+        echo "Username and Password cannot be empty!";
+    }
+    ?>
     <div class="container" style="margin-top:30px">
         <div class="row">
             <div class="col-sm-8">
                 <h2>Register</h2>
-                <!-- registration form -->
-                <form id="registration_form" method="post">
+                <form id="registration_form" action="register.php" method="post">
                     <div>
                         <label for="username">Username</label>
                         <input type="text" id="username" name="username" />
-                        <div class="error" id="usernameError">Username is required.</div>
                     </div>
 
                     <div>
                         <label for="password">Password</label>
                         <input type="password" id="password" name="password" />
-                        <div class="error" id="passwordError">Password is required.</div>
                     </div>
                     <div class="submit-button">
-                        <input type="submit" name="Login" value="Login"/>
+                        <input type="submit" name="Register" value="Register" onclick="hashpassword()" />
                     </div>
                 </form>
+                <script src="sha256.js"></script>
+                <script type="text/javascript">
+                    function hashPassword() {
+                        var input = document.getElementById('password').value;
+
+                        var hash = SHA256.hash(input);
+
+                        document.getElementById("password").innerHTML = hash;
+                        document.getElementById("password").value = hash;
+                    }
+                </script>
             </div>
         </div>
     </div>
-    <?php include 'footer.php';?>
+    <footer>
+        <?php include 'footer.php'; ?>
+    </footer>
 </body>
 
 </html>
