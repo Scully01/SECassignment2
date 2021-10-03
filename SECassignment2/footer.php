03b7dfb9e8792be06f402453a9d7961618049cbe
HOANG TRAN s3720825 <s3720825@student.rmit.edu.au>
2021-10-03 21:43:05 +1100

    Create footer.php

---
+++ SECassignment2/footer.php
@@ -0,0 +1,14 @@
+<footer>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="contactus.php">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="sitemap.php">Sitemap</a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+    </footer>
