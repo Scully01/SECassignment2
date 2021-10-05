2d3a2e34936761930e3d934e8d789445af78b28f
HOANG TRAN s3720825 <s3720825@student.rmit.edu.au>
2021-10-05 23:05:56 +1100

    Update confirm.php

--- SECassignment2/confirm.php
+++ SECassignment2/confirm.php
@@ -1,1 +1,16 @@
-<p name="encrypted" id="encrypted"></p>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // collect value of input field
+  if (isset($POST["encrypted"])) {
+  $name = $GET['encrypted'];
+  if (empty($name)) {
+    echo "Name is empty";
+  } else {
+    $myfile = fopen("database/payment.txt", "w");
+    $txt = $_GET["encrypted"];
+    fwrite($myfile, $txt);
+    fclose($myfile);
+  }
+}
+}
+?>
