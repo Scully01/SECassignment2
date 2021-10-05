0c1ace2243504aa15013705d7e062dee13e5caf0
HOANG TRAN s3720825 <s3720825@student.rmit.edu.au>
2021-10-05 23:06:27 +1100

    Update logout.php

--- SECassignment2/logout.php
+++ SECassignment2/logout.php
@@ -1,7 +1,10 @@
 <?php
-	session_start();
-	
-	unset($_SESSION['login']);
+ require_once('phpfunctions.php');
 
+    logoutUser();
     header('Location: login.php');
-?>
+    exit();
+ ?>
+
+ <!-- code for users to logout. once loggedout, page will load up onto the login page.  -->
+ <!-- a combination of code inside this php file is partly attributed  the code seen in week 8 by Shekhar Kalra and Matther Bolger from RMIT University. -->
